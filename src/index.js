62f75b3d85f04c889434742c6d2450511052c816
Nick Ford <nickforddesign@gmail.com>
2017-10-02 12:30:35 -0400

    exported request from wrong place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import makeMixin from './mixin'
+import Request from './request'
 
 export default class VueRequest {
   static install(Vue, options) {
     Vue.mixin(makeMixin(Vue, options))
   }
 }
+
+export { Request }
