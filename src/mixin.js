13338f8de0d82a72d5a93eb9d7bf82b1547ca778
Nick Ford <nickforddesign@gmail.com>
2017-10-02 17:00:25 -0400

    added vm parameter to before hooks

--- src/mixin.js
+++ src/mixin.js
@@ -18,7 +18,7 @@ const init = async (vm, _config) => {
   const config = _.merge({}, defaults, _config)
   vm.$request = async (url, options) => {
     if (typeof config.before === 'function') {
-      await config.before()
+      await config.before(vm)
     }
     return Request(url, options, config)
   }
