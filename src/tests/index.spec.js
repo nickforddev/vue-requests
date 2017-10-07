46c4d469d759f8623a20f3111d37d70019cd646f
Nick Ford <nickforddesign@gmail.com>
2017-10-07 14:32:06 -0400

    improved tests

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -3,10 +3,20 @@ import Vue from 'vue'
 
 import { sleep } from '../utils'
 
+Vue.config.productionTip = false
+
+// spy on hoks
 const spyBefore = jest.fn()
 const spyTimeout = jest.fn()
 
+// set up VueRequests plugin
 Vue.use(VueRequests, {
+  headers: {
+    one: 'test1',
+    two() {
+      return 'test2'
+    }
+  },
   before() {
     spyBefore(this)
   },
@@ -17,6 +27,12 @@ Vue.use(VueRequests, {
 })
 
 const app = new Vue()
+
+// spy on app.$request
+const request = app.$request
+app.$request = jest.fn((...args) => {
+  return request(...args)
+})
 
 describe('vue-requests', () => {
   fetch.mockResponseOnce(JSON.stringify({
@@ -36,6 +52,16 @@ describe('vue-requests', () => {
       })
   })
 
+  it('should be passing the correct args', () => {
+    expect(app.$request.mock.calls[0])
+      .toEqual(['/'])
+  })
+
+  it('fetch should have received correct method GET', () => {
+    expect(global.fetch.mock.calls[0][1].method)
+      .toBe('GET')
+  })
+
   it('should fire the before hook', () => {
     expect(spyBefore.mock.calls.length)
       .toBe(1)
@@ -44,6 +70,43 @@ describe('vue-requests', () => {
   it('the before hook should have the vue instance applied', () => {
     expect(spyBefore.mock.calls[0][0])
       .toBeInstanceOf(Vue)
+  })
+
+  fetch.mockResponseOnce(JSON.stringify({
+    'foo': 'bar'
+  }), {
+    status: 200
+  })
+
+  it('fetch should receive correct method POST', () => {
+    expect.assertions(1)
+    return app.$request('/', {
+      method: 'POST',
+      body: {
+        test: true
+      }
+    })
+      .then(() => {
+        expect(global.fetch.mock.calls[1][1].method)
+          .toBe('POST')
+      })
+  })
+
+  it('fetch should receive correct body', () => {
+    expect(global.fetch.mock.calls[1][1].body)
+      .toEqual(JSON.stringify({
+        test: true
+      }))
+  })
+
+  it('fetch should receive correct header passed as property: one', () => {
+    expect(global.fetch.mock.calls[1][1].headers.map.one)
+      .toEqual(['test1'])
+  })
+
+  it('fetch should receive correct header passed as function: two', () => {
+    expect(global.fetch.mock.calls[1][1].headers.map.two)
+      .toEqual(['test2'])
   })
 
   fetch.mockResponseOnce(JSON.stringify({
