994c2cf6a0a44f2ba2aaf48ac096d8202037947b
Nick Ford <nickforddesign@gmail.com>
2018-03-29 12:29:24 -0400

    fixed demo

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import App from './demo'
 // import VueRequest from '../../dist/vue-requests.js'
-import VueRequests from '../index'
+import VueRequest from '../index'
 
-// const { port } = require('./config')
+const { port } = require('./config')
 
 Vue.config.productionTip = false
 
-// const token = 'test'
+const token = 'test'
 
-Vue.use(VueRequests)
+Vue.use(VueRequest, {
+  root: `http://localhost:${port}`,
+  headers: {
+    Access() {
+      return token
+    },
+    Refresh: 'test_refresh_token'
+  },
+  before() {
+    console.log('fire this before')
+  },
+  timeout() {
+    console.log('fire this on timeout')
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
